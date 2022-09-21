a60a41801e813eec0df3bf9a0fcd216c5d5f0151
face0ff <facekrakadil@gmail.com>
2022-09-21 16:12:08 +0200

    gg

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+<<<<<<< Updated upstream
 11111
+=======
+print("heelo git")
+tryrtytrytry
+
+rtfbt
+
+eterterter
+<<<<<<< Updated upstream
+=======
+retertertert
+fjghfjgj22222222222222222222222222222222
+
+
+11111111111111111111111111
+ewfewf
+dsfsdfsdfsdf
+
+
+222222222222222222222222222222
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
